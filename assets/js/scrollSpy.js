6d0e9d6c8adfbe9920c74bb6f4c0c4862ac28ea8
JGabrielFDC <jgabrielfdc08@gmail.com>
2025-07-01 13:55:27 -0300

    :sparkles: Auto Script Tag

--- assets/js/scrollSpy.js
+++
@@ -1,8 +0,0 @@
-/*document.onscroll=()=>{
-    if((document.getElementById('competencias').offsetTop-400)<=window.scrollY && (document.getElementById('competencias').offsetTop+200)>=window.scrollY){
-        document.getElementById('competencias').className="slide-out";
-    }else{
-        document.getElementById('competencias').className="slide-in";
-
-    }
-}*/
